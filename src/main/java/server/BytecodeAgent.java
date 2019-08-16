aaebf7f1015ddbee02fc83bf0f569f09d4e94e66
Sanjay Govind <govindsanj@ecs.vuw.ac.nz>
2019-08-17 00:36:52 +1200

    And we have the agent poll the registry, and so if the registry dies, it frees ports.

--- src/main/java/server/BytecodeAgent.java
+++ src/main/java/server/BytecodeAgent.java
@@ -2,9 +2,11 @@ package server;
 
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.Instrumentation;
+import java.rmi.NoSuchObjectException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 import java.security.ProtectionDomain;
 import java.util.HashMap;
 
@@ -24,6 +26,19 @@ public class BytecodeAgent implements ClassFileTransformer {
         instrumentation.addTransformer(new BytecodeAgent());
         Registry registry = LocateRegistry.getRegistry(registryPort);
         registry.rebind("bytecodeLookup", server);
+        new Thread(()->{
+            try {
+                registry.list();
+                Thread.sleep(100);
+            } catch (RemoteException | InterruptedException ignored) {
+                //time to assume that the process has died and that is why we have lost access to the registry
+                try {
+                    UnicastRemoteObject.unexportObject(server, true);
+                } catch (NoSuchObjectException ignored2) {
+                   //If the object isn't shared, we don't care, chances are the main process has tidied things up already.
+                }
+            }
+        }).start();
     }
 
     @Override
