01e69f71e7fc74ffc08862f504a2a1cc22bd00ce
sanjay900 <sanjay.govind9@gmail.com>
2019-09-28 01:05:16 +1200

    Update the names of some tests

--- src/test/java/TestsDirect.java
+++ src/test/java/TestsDirect.java
@@ -7,20 +7,6 @@ import java.time.Instant;
 
 public class TestsDirect {
     @Test
-    public void timeConstruction() {
-        int testCount = 20;
-        long totalTime = 0L;
-        for (int i = 0; i < testCount + 5; i++) {
-            Instant start = Instant.now();
-            new Tests.TimingTest();
-            Instant end = Instant.now();
-            if (i > 5) {
-                totalTime += Duration.between(start, end).toMillis();
-            }
-        }
-        System.out.println("Time taken to construct: " + totalTime / testCount);
-    }
-    @Test
     public void timeExecution() {
         int testCount = 10;
         long expected = 49999995000000L;
