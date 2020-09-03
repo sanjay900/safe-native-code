bcabb623435a7ad56a1ea4c50cacf927691ea071
Sanjay Govind <sanjay.govind9@gmail.com>
2020-09-03 20:14:33 +1200

    Simplify processclassloader

--- src/main/java/safeNativeCode/slave/process/ProcessClassloader.java
+++ src/main/java/safeNativeCode/slave/process/ProcessClassloader.java
@@ -19,17 +19,6 @@ import java.util.List;
  */
 public class ProcessClassloader extends SecureClassLoader {
     private static IClassSupplier bytecodeSupplier;
-    //A list of classes that we need to load using a ProcessClassloader.
-    //This is needed so that all any code run on the client, will use this classloader as its parent for loading classes.
-    private List<String> forced = Arrays.asList(
-            Functions.class.getName() + "($.*)?",
-            Process.class.getName(),
-            ProcessObject.class.getName(),
-            ProcessMain.class.getName(),
-            InternalSlave.class.getName(),
-            RemoteObject.class.getName(),
-            SlaveException.class.getName()
-    );
 
     public ProcessClassloader(ClassLoader parent) {
         super(parent);
@@ -37,30 +26,17 @@ public class ProcessClassloader extends SecureClassLoader {
 
     @Override
     public Class<?> loadClass(String name) throws ClassNotFoundException {
-        try {
-            String className = name.replace(".", File.separator) + ".class";
-            for (String s : forced) {
-                if (name.matches(s)) {
-                    if (forced.stream().anyMatch(name::matches)) {
-                        // Reload these specific classes using this classloader instead of the app classloader.
-                        InputStream is = getResourceAsStream(className);
-                        if (is == null) {
-                            throw new ClassNotFoundException("Unable to find: " + name);
-                        }
-                        byte[] b = Utils.readStream(is);
-                        return super.defineClass(name, b, 0, b.length);
-                    }
-                }
+        if (bytecodeSupplier != null) {
+            try {
+                byte[] b = bytecodeSupplier.getByteCode(name);
+                if (b == null || Utils.isJavaClass(name) || name.startsWith("safeNativeCode.")) return super.loadClass(name);
+                return super.defineClass(name, b, 0, b.length);
+            } catch (IOException e) {
+                // If we lose connection to the main JVM, just throw a class not found exception.
+                throw new ClassNotFoundException("Unable to load: " + name, e);
             }
-
-            if (Utils.isJavaClass(name) || bytecodeSupplier == null) return super.loadClass(name);
-            byte[] b = bytecodeSupplier.getByteCode(name);
-            if (b == null) return super.loadClass(name);
-            return super.defineClass(name, b, 0, b.length);
-        } catch (IOException e) {
-            // If we lose connection to the main JVM, just throw a class not found exception.
-            throw new ClassNotFoundException("Unable to load: " + name, e);
         }
+        return super.loadClass(name);
     }
 
     // Due to the fact that this is used across modules (ProcessClassloader and ProcessSlave exist inside different ClassLoaders), we need to make it public.
